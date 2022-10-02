689074839a2e567b44c2af72079aedfc08202235
Barbara Litwińska <litwinska.barbara@gmail.com>
2022-10-02 14:16:25 +0000

    Super app

--- picka.rb
+++ picka.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'yaml/store'
 
 Choices = {
   "pizza" => "Pizza",
@@ -10,4 +11,33 @@ Choices = {
 get '/' do
   @title = "Co tam sobie dzisiaj zjemy?"
   erb :index
+end
+
+post '/cast' do
+  @title = "Tutaj będzie co wybrałeś"
+  @choice = Choices[params["vote"]]
+  @vote = params["vote"]
+  @store = YAML::Store.new 'votes.yml'
+
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
+
+  erb :cast
+end
+
+get '/results' do
+  @title = "Nasze wyniczki! Mniam mniam"
+  @store = YAML::Store.new 'votes.yml'
+
+  @votes = @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes']
+  end
+
+  puts @votes
+
+  erb :results
 end
